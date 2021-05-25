0aa5192e4fcd738d2bbc5f7b34545e7b7288f254
kyuwonii <cathy0123@naver.com>
2021-05-25 23:16:18 +0900

    로그인/메인 2차 수정, 부트스트랩 설정

--- vendor/js/ko.js
+++ vendor/js/ko.js
@@ -1,1 +1,41 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t(require("moment"),require("fullcalendar")):"function"==typeof define&&define.amd?define(["moment","fullcalendar"],t):"object"==typeof exports?t(require("moment"),require("fullcalendar")):t(e.moment,e.FullCalendar)}("undefined"!=typeof self?self:this,function(e,t){return function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}var n={};return t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=151)}({0:function(t,n){t.exports=e},1:function(e,n){e.exports=t},151:function(e,t,n){Object.defineProperty(t,"__esModule",{value:!0}),n(152);var r=n(1);r.datepickerLocale("ko","ko",{closeText:"닫기",prevText:"이전달",nextText:"다음달",currentText:"오늘",monthNames:["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],monthNamesShort:["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],dayNames:["일요일","월요일","화요일","수요일","목요일","금요일","토요일"],dayNamesShort:["일","월","화","수","목","금","토"],dayNamesMin:["일","월","화","수","목","금","토"],weekHeader:"주",dateFormat:"yy. m. d.",firstDay:0,isRTL:!1,showMonthAfterYear:!0,yearSuffix:"년"}),r.locale("ko",{buttonText:{month:"월",week:"주",day:"일",list:"일정목록"},allDayText:"종일",eventLimitText:"개",noEventsMessage:"일정이 없습니다"})},152:function(e,t,n){!function(e,t){t(n(0))}(0,function(e){return e.defineLocale("ko",{months:"1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월".split("_"),monthsShort:"1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월".split("_"),weekdays:"일요일_월요일_화요일_수요일_목요일_금요일_토요일".split("_"),weekdaysShort:"일_월_화_수_목_금_토".split("_"),weekdaysMin:"일_월_화_수_목_금_토".split("_"),longDateFormat:{LT:"A h:mm",LTS:"A h:mm:ss",L:"YYYY.MM.DD.",LL:"YYYY년 MMMM D일",LLL:"YYYY년 MMMM D일 A h:mm",LLLL:"YYYY년 MMMM D일 dddd A h:mm",l:"YYYY.MM.DD.",ll:"YYYY년 MMMM D일",lll:"YYYY년 MMMM D일 A h:mm",llll:"YYYY년 MMMM D일 dddd A h:mm"},calendar:{sameDay:"오늘 LT",nextDay:"내일 LT",nextWeek:"dddd LT",lastDay:"어제 LT",lastWeek:"지난주 dddd LT",sameElse:"L"},relativeTime:{future:"%s 후",past:"%s 전",s:"몇 초",ss:"%d초",m:"1분",mm:"%d분",h:"한 시간",hh:"%d시간",d:"하루",dd:"%d일",M:"한 달",MM:"%d달",y:"일 년",yy:"%d년"},dayOfMonthOrdinalParse:/\d{1,2}(일|월|주)/,ordinal:function(e,t){switch(t){case"d":case"D":case"DDD":return e+"일";case"M":return e+"월";case"w":case"W":return e+"주";default:return e}},meridiemParse:/오전|오후/,isPM:function(e){return"오후"===e},meridiem:function(e,t,n){return e<12?"오전":"오후"}})})}})});
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t(require("moment"),require("fullcalendar"))
+:"function"==typeof define&&define.amd?define(["moment","fullcalendar"],t)
+:"object"==typeof exports?t(require("moment"),require("fullcalendar"))
+:t(e.moment,e.FullCalendar)}("undefined"!=typeof self?self:this,function(e,t)
+{return function(e)
+    {function t(r){if(n[r])
+        return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};
+        return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}var n={};
+        return t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},
+        t.n=function(e){var n=e&&e.__esModule?function(){
+            return e.default}:function(){
+                return e
+            };
+            return t.d(n,"a",n),n},t.o=function(e,t){
+                return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=151)}
+                ({0:function(t,n){t.exports=e},
+                1:function(e,n){e.exports=t},
+                151:function(e,t,n){
+                    Object.defineProperty(t,"__esModule",{value:!0}),n(152);var r=n(1);
+                    r.datepickerLocale("ko","ko",{closeText:"close",prevText:"Previous",nextText:"Next",currentText:"Today", weekends:"weekend",
+                    monthNames:["January","February","March","April","May","June","July","August","September","October","November","December"],
+                    monthNamesShort:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+                    dayNames:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+                    dayNamesShort:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],
+                    dayNamesMin:["S","M","T","W","T","F","S"],
+                    weekHeader:"Week",dateFormat:"yy. m. d.",
+                    firstDay:0,isRTL:!1,showMonthAfterYear:!0,yearSuffix:""}),
+                    r.locale("ko",{buttonText:{weekends:"weekend",month:"month",week:"week",day:"day",list:"list"},
+                    allDayText:"allDay",eventLimitText:".",noEventsMessage:"No Events"})},
+                152:function(e,t,n){!function(e,t){t(n(0))}(0,
+                    function(e){return e.defineLocale("ko",{months:"January_February_March_April_May_June_July_August_September_October_November_December".split("_"),
+                    monthsShort:"Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec".split("_"),
+                    weekdays:"Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday".split("_"),
+                    weekdaysShort:"Sun_Mon_Tue_Wed_Thu_Fri_Sat".split("_"),
+                    weekdaysMin:"S_M_T_W_T_F_S".split("_"),
+                    weekends:"weekend",
+                    longDateFormat:{LT:"A h:mm",LTS:"A h:mm:ss",L:"YYYY.MM.DD.",LL:"YYYY.MM.DD.",LLL:"YYYY.MM.DD. A h:mm",LLLL:"YYYY.MM.DD. dddd A h:mm",l:"YYYY.MM.DD.",ll:"YYYY.MM.DD.",lll:"YYYY.MM.DD. A h:mm",llll:"YYYY.MM.DD. dddd A h:mm"},
+                    calendar:{sameDay:"Today LT",nextDay:"Tomorrow LT",nextWeek:"dddd LT",lastDay:"Yesterday LT",lastWeek:"lastWeek dddd LT",sameElse:"L"},
+                    relativeTime:{future:"%s after",past:"%s before",s:"1 second",ss:"%d second",m:"1 minute",mm:"%d minute",h:"One Hour",hh:"%d hour",d:"One Day",dd:"%d Day",M:"One Month",MM:"%d Month",y:"One Year",yy:"%d Year"},
+                    dayOfMonthOrdinalParse:/\d{1,2}(Day|Month|Week)/,ordinal:
+                    function(e,t){switch(t){case"d":case"D":case"DDD":return e+"Day";case"M":return e+"Month";case"w":case"W":return e+"Week";default:return e}},meridiemParse:/AM|PM/,isPM:function(e){return"PM"===e},meridiem:function(e,t,n){return e<12?"AM":"PM"}})})}})});
